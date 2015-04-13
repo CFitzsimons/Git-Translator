860765f45a66fbd658d0d2891212dd9b9441ad81
Colin Fitzsimons <colin.altered@gmail.com>
2015-04-13 13:00:31 +0100

    translation

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,74 +1,117 @@
 import java.io.*;
 import java.util.*;
+import com.gtranslate.text.*;
+import com.gtranslate.parsing.*;
+import com.gtranslate.Translator;
+import com.gtranslate.Language;
 
 public class EntryPoint {
 	public static void main(String[] args) throws Exception {
-
-    ArrayList<LogEntry> logs = getLogs();
-
-    for(LogEntry e : logs){
-      System.out.println(e);
-    }
+		GitShell shell = new GitShell(new File("C:/Users/Colin/Documents/GitHub/lang_german"));
+		File logFile = new File("logs.txt");
+		shell.generateLogs(logFile);
+    ArrayList<LogEntry> logs = Reader.getLogsFromFile(logFile);
+		Stuff late = new Stuff();
+		late.translateLogs(logs);
+    //for(LogEntry e : logs){
+    //  System.out.println(e);
+    //}
 
 
 	}
 
-  public static ArrayList<LogEntry> getLogs()throws IOException{
-    runCommand("git log", "logout.txt");
-    Scanner scan = new Scanner(new File("logout.txt"));
-    String currLine = "";
-    ArrayList <LogEntry> logs = new ArrayList<>();
-    System.out.println(scan.hasNextLine());
-    LogEntry curr = null;
-    while (scan.hasNextLine()) {
-      currLine = scan.nextLine();
-      String[] words = currLine.split(" ");
-      if (words.length <= 0) continue; //Empty line, skip
+}
 
-      if (words[0].equals("commit")) {
-        if(curr != null){
-            logs.add(curr);
-        }
-        curr = new LogEntry(words[1]);
-      }else if(words[0].contains("Author")){
-        curr.addAuthor(build(Arrays.copyOfRange(words, 1, words.length-1)));
-      }else if(words[0].contains("Date")){
-        curr.addDate(build(Arrays.copyOfRange(words, 1, words.length-1)));
-      }else
-        curr.addLine(currLine);
+class Stuff{
 
+	public Stuff(){
 
-
-    }
-    return logs;
-  }
-	public static void runCommand(String command, String outputName) {
-
-		try {
-			PrintWriter pw = new PrintWriter(new File(outputName));
-			String line;
-			Process p = Runtime.getRuntime().exec(command, null, new File("C:\\Users\\Colin\\Documents\\Github\\lang_german"));
-
-			BufferedReader in = new BufferedReader(
-			new InputStreamReader(p.getInputStream()));
-			while ((line = in .readLine()) != null) {
-				pw.println(line);
-        //System.out.println(line);
-			} in.close(); pw.flush(); pw.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+	}
+		public void translateLogs(ArrayList<LogEntry> allLogs){
+			Translator translate = Translator.getInstance();
+			String text = translate.translate("Hello!", Language.ENGLISH, Language.ROMANIAN);
+			System.out.println(text); // "Bună ziua!"
 		}
 
 
-	}
-  private static String build(String [] array){
-    StringBuilder builder = new StringBuilder();
-    for(String s : array) {
-        builder.append(s + " ");
-    }
-    return builder.toString();
-  }
 }
+
+class Reader{
+	public static ArrayList<LogEntry> getLogsFromFile(File logFile)throws IOException{
+		Scanner scan = new Scanner(logFile);
+
+		String currLine;
+		ArrayList <LogEntry> logs = new ArrayList<>();
+		LogEntry curr = null;
+
+		while (scan.hasNextLine()) {
+			currLine = scan.nextLine();
+			String[] words = currLine.split(" ");
+			if (words.length <= 0) continue; //Empty line, skip
+
+			if (words[0].equals("commit")) {
+				if(curr != null){
+						logs.add(curr);
+				}
+				curr = new LogEntry(words[1]);
+			}else if(words[0].equals("Author:")){
+				curr.addAuthor(build(Arrays.copyOfRange(words, 1, words.length-1)));
+			}else if(words[0].equals("Date:")){
+				curr.addDate(build(Arrays.copyOfRange(words, 1, words.length-1)));
+			}else
+				curr.addLine(currLine);
+
+
+
+		}
+		return logs;
+	}
+	private static String build(String [] array){
+		StringBuilder builder = new StringBuilder();
+		for(String s : array) {
+				builder.append(s + " ");
+		}
+		return builder.toString();
+	}
+}
+
+class GitShell{
+	private File target;
+
+	public GitShell(File targetRepo){
+		this.target = targetRepo;
+	}
+
+	private static Process executeNonCommand(String cmd, File loc){
+		try{
+			return Runtime.getRuntime().exec(cmd, null, loc);
+		}catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return null;
+
+	}
+	//new File("C:\\Users\\Colin\\Documents\\Github\\lang_german")
+
+	public void generateLogs(File out){
+		try {
+			PrintWriter printer = new PrintWriter(out);
+			String line;
+			Process p = executeNonCommand("git log", target);
+
+			BufferedReader in = new BufferedReader(
+				new InputStreamReader(p.getInputStream()));
+			while ((line = in.readLine()) != null) {
+				printer.println(line);
+				//System.out.println(line);
+			} in.close(); printer.flush(); printer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
 
 class LogEntry {
 	private String sha;
